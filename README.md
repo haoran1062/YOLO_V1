51110680c9c08f7fbce3f7fb0302d58ddc0968bf
haoran <haoran1062@163.com>
2019-04-04 19:34:30 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # YOLO_V1
+### ReadMe 
+ - this repo is an easy implementation for YOLO V1 with **Pytorch 0.4** and **python3.6**
+ - still coding... now, **YOLO V1 loss**, **Resnet backbone** finished. 
+ - now I'm writing the fuckin **data loader**
+
+
+
--- tensor_test.py
+++ tensor_test.py
@@ -135,96 +135,6 @@ def simluate_pred_tensor(pred_tensor, target_tensor, batch_size, S, B, C, lbd_co
     total_loss /= batch_size
     print('location loss : ', location_loss, 'contain loss : ', contain_loss, 'not contain loss: ', not_contain_loss  , 'no obj loss : ', no_obj_loss, 'cls loss : ', cls_loss)
     print(total_loss)
-
-
-
-def forward(pred_tensor, target_tensor, l_coord=5, l_noobj=.5):
-        '''
-        pred_tensor: (tensor) size(batchsize,S,S,Bx5+20=30) [x,y,w,h,c]
-        target_tensor: (tensor) size(batchsize,S,S,30)
-        '''
-        N = pred_tensor.size()[0]
-        coo_mask = target_tensor[:,:,:,4] > 0
-        noo_mask = target_tensor[:,:,:,4] == 0
-        coo_mask = coo_mask.unsqueeze(-1).expand_as(target_tensor)
-        noo_mask = noo_mask.unsqueeze(-1).expand_as(target_tensor)
-        # print(coo_mask.shape, noo_mask.shape)
-        # print(coo_mask)
-
-        coo_pred = pred_tensor[coo_mask].view(-1,30)
-        box_pred = coo_pred[:,:10].contiguous().view(-1,5) #box[x1,y1,w1,h1,c1]
-        class_pred = coo_pred[:,10:]                       #[x2,y2,w2,h2,c2]
-        
-        coo_target = target_tensor[coo_mask].view(-1,30)
-        box_target = coo_target[:,:10].contiguous().view(-1,5)
-        class_target = coo_target[:,10:]
-
-        # compute not contain obj loss
-        noo_pred = pred_tensor[noo_mask].view(-1,30)
-        noo_target = target_tensor[noo_mask].view(-1,30)
-        noo_pred_mask = torch.cuda.ByteTensor(noo_pred.size())
-        noo_pred_mask.zero_()
-        # print(noo_pred_mask.shape)
-        noo_pred_mask[:,4]=1;noo_pred_mask[:,9]=1
-        noo_pred_c = noo_pred[noo_pred_mask] #noo pred只需要计算 c 的损失 size[-1,2]
-        noo_target_c = noo_target[noo_pred_mask]
-        nooobj_loss = F.mse_loss(noo_pred_c,noo_target_c,size_average=False)
-
-        #compute contain obj loss
-        coo_response_mask = torch.cuda.ByteTensor(box_target.size())
-        coo_response_mask.zero_()
-        coo_not_response_mask = torch.cuda.ByteTensor(box_target.size())
-        coo_not_response_mask.zero_()
-        box_target_iou = torch.zeros(box_target.size())
-        for i in range(0,box_target.size()[0],2): #choose the best iou box
-            box1 = box_pred[i:i+2]
-            box1_xyxy = torch.FloatTensor(box1.size())
-            box1_xyxy[:,:2] = box1[:,:2]/14. -0.5*box1[:,2:4]
-            box1_xyxy[:,2:4] = box1[:,:2]/14. +0.5*box1[:,2:4]
-            box2 = box_target[i].view(-1,5)
-            box2_xyxy = torch.FloatTensor(box2.size())
-            box2_xyxy[:,:2] = box2[:,:2]/14. -0.5*box2[:,2:4]
-            box2_xyxy[:,2:4] = box2[:,:2]/14. +0.5*box2[:,2:4]
-            iou = compute_iou_matrix(box1_xyxy[:,:4],box2_xyxy[:,:4]) #[2,1]
-            max_iou,max_index = iou.max(0)
-            max_index = max_index.data
-            # print(iou)
-            # print(box1_xyxy[:, :4])
-            # print(box1[:, :4])
-            
-            coo_response_mask[i+max_index]=1
-            coo_not_response_mask[i+1-max_index]=1
-
-            #####
-            # we want the confidence score to equal the
-            # intersection over union (IOU) between the predicted box
-            # and the ground truth
-            #####
-            box_target_iou[i+max_index,torch.LongTensor([4])] = (max_iou).data
-        # box_target_iou = Variable(box_target_iou).cuda()
-        # print(coo_response_mask[:, 0])
-        #1.response loss
-        box_pred_response = box_pred[coo_response_mask].view(-1,5)
-        box_target_response_iou = box_target_iou[coo_response_mask].view(-1,5)
-        box_target_response = box_target[coo_response_mask].view(-1,5)
-        contain_loss = F.mse_loss(box_pred_response[:,4],box_target_response_iou[:,4],size_average=False)
-        loc_loss = F.mse_loss(box_pred_response[:,:2],box_target_response[:,:2],size_average=False) + F.mse_loss(torch.sqrt(box_pred_response[:,2:4]),torch.sqrt(box_target_response[:,2:4]),size_average=False)
-        #2.not response loss
-        box_pred_not_response = box_pred[coo_not_response_mask].view(-1,5)
-        box_target_not_response = box_target[coo_not_response_mask].view(-1,5)
-        box_target_not_response[:,4]= 0
-        #not_contain_loss = F.mse_loss(box_pred_response[:,4],box_target_response[:,4],size_average=False)
-        
-        #I believe this bug is simply a typo
-        not_contain_loss = F.mse_loss(box_pred_not_response[:,4], box_target_not_response[:,4],size_average=False)
-
-        #3.class loss
-        class_loss = F.mse_loss(class_pred,class_target,size_average=False)
-
-        total_loss = (l_coord*loc_loss + 2*contain_loss + not_contain_loss + l_noobj*nooobj_loss + class_loss)/N
-        print('location loss : ', loc_loss, 'contain loss : ', contain_loss, 'not contain loss: ', not_contain_loss , 'no obj loss : ', nooobj_loss, 'cls loss : ', class_loss)
-
-        print(total_loss)
     
 
 
@@ -239,14 +149,12 @@ if __name__ == "__main__":
 
     simluate_pred_tensor(pred_tensor, target_tensor, batch_size, S, B, C)
 
-    o_pred_tensor = convert_input_tensor_dim(pred_tensor)
-    o_target_tensor = convert_input_tensor_dim(target_tensor)
+    # o_pred_tensor = convert_input_tensor_dim(pred_tensor)
+    # o_target_tensor = convert_input_tensor_dim(target_tensor)
 
     # print(o_pred_tensor.shape, o_target_tensor.shape)
     # print(o_pred_tensor[:, 0, 0, :10])
     # print(pred_tensor[:, 0, 0, :10])
-
-    forward(o_pred_tensor, o_target_tensor)
 
     # [batch_szie, S, S, B*5+C]  [Conf1, Conf2, xywh1, xywh2, Cls] -> [Conf1, xywh1, Conf2, xywh2, Cls]
     
